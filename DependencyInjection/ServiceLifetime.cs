f9d0fb5335b83cfc8b9cc9e34474fe55c0835a23
lq <Administrator@LST-RD-SFM>
2020-09-30 10:15:53 +0800

    first commit

---
+++ DependencyInjection/ServiceLifetime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjection
+{
+   public enum ServiceLifetime : sbyte
+    {
+        /// <summary>
+        /// Specifies that a single instance of the service will be created.
+        /// </summary>
+        Singleton = 0,
+
+        /// <summary>
+        /// Specifies that a new instance of the service will be created for each scope.
+        /// </summary>
+        Scoped = 1,
+
+        /// <summary>
+        /// Specifies that a new instance of the service will be created every time it is requested.
+        /// </summary>
+        Transient = 2,
+    }
+}
